35fbaf9091bbe63ff8933aef543c4211f0a55c4e
Quentin Menini <quentin.menini@gmail.com>
2016-04-19 21:00:25 +0200

    Removed resize and added blur

--- app/src/main/java/com/example/q/renderscriptexample/EditPictureActivity.java
+++ app/src/main/java/com/example/q/renderscriptexample/EditPictureActivity.java
@@ -1,8 +1,8 @@
 package com.example.q.renderscriptexample;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +13,6 @@ import android.widget.TextView;
 
 import com.example.q.renderscriptexample.utils.RenderScriptImageEdit;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
@@ -25,6 +23,7 @@ public class EditPictureActivity extends AppCompatActivity {
 
     private FloatingActionButton mFloatingActionButton;
     private TextView mTextView;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,63 +43,75 @@ public class EditPictureActivity extends AppCompatActivity {
             finish();
         }
         final ImageView iv_original_image = (ImageView) findViewById(R.id.original_image);
-        final ImageView mImageView = (ImageView) findViewById(R.id.computed_image);
+        mImageView = (ImageView) findViewById(R.id.computed_image);
         mTextView = (TextView) findViewById(R.id.time_textview);
         mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
         iv_original_image.setImageBitmap(image);
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                if (image.getWidth()<1000 && image.getHeight()<1000) {
-                    image = RenderScriptImageEdit.scaleBitmap(image, 2.0f, EditPictureActivity.this);
-                    iv_original_image.setImageBitmap(image);
-                }
-                Date begin = new Date();
-                editedImage = RenderScriptImageEdit.histogramEqualization(image, EditPictureActivity.this);
-                Date end = new Date();
-                long time = end.getTime()-begin.getTime();
-                String text = getString(R.string.compute_time) + time + "ms for "+image.getWidth()+"x"+image.getHeight()+" pixels";
-                mTextView.setText(text);
-                mImageView.setImageBitmap(editedImage);
-                setFAB();
-            }
-        };
-        runnable.run();
+        new HistogramEqualizationTask().execute();
+
     }
 
-    private void setFAB() {
-        mFloatingActionButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        shareImage();
+    private void setFAB(Boolean blur) {
+        if (blur) {
+            mFloatingActionButton.setVisibility(View.GONE);
+        }
+        else {
+            mFloatingActionButton.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            new BlurTask().execute();
+                        }
                     }
-                }
-        );
-        mFloatingActionButton.setVisibility(View.VISIBLE);
+            );
+            mFloatingActionButton.setVisibility(View.VISIBLE);
+        }
     }
 
-    private void shareImage() {
-        // Save this bitmap to a file.
-        File cache = getApplicationContext().getExternalCacheDir();
-        File sharefile = new File(cache, "renderScriptShare.png");
-        try {
-            FileOutputStream out = new FileOutputStream(sharefile);
-            editedImage.compress(Bitmap.CompressFormat.PNG, 100, out);
-            out.flush();
-            out.close();
-        } catch (IOException e) {
+    private void setEditedImageView(long time) {
+        String text = getString(R.string.compute_time) + time + "ms for "+image.getWidth()+"x"+image.getHeight()+" pixels";
+        mTextView.setText(text);
+        mImageView.setImageBitmap(editedImage);
+        setFAB(false);
+    }
 
+    private void setBlurredImage(long time) {
+        String text = mTextView.getText().toString();
+        text += " " + getString(R.string.blur_compute_time) + time + "ms";
+        mTextView.setText(text);
+        setFAB(true);
+    }
+
+    private class HistogramEqualizationTask extends AsyncTask<Void, Void, Long> {
+
+        @Override
+        protected Long doInBackground(Void... params) {
+            Date begin = new Date();
+            editedImage = RenderScriptImageEdit.histogramEqualization(image, EditPictureActivity.this);
+            Date end = new Date();
+            long time = end.getTime()-begin.getTime();
+            return time;
         }
 
-        // Now send it out to share
-        Intent share = new Intent(android.content.Intent.ACTION_SEND);
-        share.setType("image/*");
-        share.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + sharefile));
-        try {
-            startActivity(Intent.createChooser(share, "Share photo"));
-        } catch (Exception e) {
+        protected void onPostExecute(Long result) {
+            setEditedImageView(result);
+        }
+    }
 
+    private class BlurTask extends AsyncTask<Void, Void, Long> {
+
+        @Override
+        protected Long doInBackground(Void... params) {
+            Date begin = new Date();
+            //editedImage = RenderScriptImageEdit.blurBitmap(editedImage, 25.0f, EditPictureActivity.this);
+            editedImage = RenderScriptImageEdit.fastblur(editedImage, 1, 25);
+            Date end = new Date();
+            long time = end.getTime()-begin.getTime();
+            return time;
+        }
+
+        protected void onPostExecute(Long result) {
+            setBlurredImage(result);
         }
     }
 }
--- app/src/main/java/com/example/q/renderscriptexample/utils/RenderScriptImageEdit.java
+++ app/src/main/java/com/example/q/renderscriptexample/utils/RenderScriptImageEdit.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
 import android.support.v8.renderscript.ScriptIntrinsicResize;
 import android.support.v8.renderscript.Type;
 
@@ -22,45 +23,37 @@ public final class RenderScriptImageEdit {
         //private constructor for utility class
     }
 
-    public static Bitmap scaleBitmap(Bitmap bitmap, float scale, Context context) {
-        //Get image size
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-
+    public static Bitmap blurBitmap(Bitmap bitmap, float radius, Context context) {
         //Create renderscript
         RenderScript rs = RenderScript.create(context);
 
         //Create allocation from Bitmap
         Allocation allocation = Allocation.createFromBitmap(rs, bitmap);
 
-        //Get new image size
-        int newWidth = (int) (width * scale);
-        int newHeight = (int) (height * scale);
+        Type t = allocation.getType();
 
-        //Create type for new image
-        Type t = Type.createXY(rs, allocation.getElement(), newWidth, newHeight);
-        //Create allocation from type
-        Allocation scaledAllocation = Allocation.createTyped(rs, t);
+        //Create allocation with the same type
+        Allocation blurredAllocation = Allocation.createTyped(rs, t);
 
         //Create script
-        ScriptIntrinsicResize scaleScript = ScriptIntrinsicResize.create(rs);
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        //Set blur radius (maximum 25.0)
+        blurScript.setRadius(radius);
         //Set input for script
-        scaleScript.setInput(allocation);
+        blurScript.setInput(allocation);
         //Call script for output allocation
-        scaleScript.forEach_bicubic(scaledAllocation);
+        blurScript.forEach(blurredAllocation);
 
-        //Create new bitmap
-        Bitmap res = Bitmap.createBitmap(newWidth, newHeight, bitmap.getConfig());
         //Copy script result into bitmap
-        scaledAllocation.copyTo(res);
+        blurredAllocation.copyTo(bitmap);
 
         //Destroy everything to free memory
         allocation.destroy();
-        scaledAllocation.destroy();
-        scaleScript.destroy();
+        blurredAllocation.destroy();
+        blurScript.destroy();
         t.destroy();
 
-        return res;
+        return bitmap;
     }
 
     public static Bitmap histogramEqualization(Bitmap image, Context context) {
@@ -130,6 +123,213 @@ public final class RenderScriptImageEdit {
         rs.destroy();
 
         return res;
+    }
+
+    //TODO remove
+    public static Bitmap fastblur(Bitmap sentBitmap, float scale, int radius) {
+
+        int width = Math.round(sentBitmap.getWidth() * scale);
+        int height = Math.round(sentBitmap.getHeight() * scale);
+        sentBitmap = Bitmap.createScaledBitmap(sentBitmap, width, height, false);
+
+        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
+
+        if (radius < 1) {
+            return (null);
+        }
+
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+
+        int wm = w - 1;
+        int hm = h - 1;
+        int wh = w * h;
+        int div = radius + radius + 1;
+
+        int r[] = new int[wh];
+        int g[] = new int[wh];
+        int b[] = new int[wh];
+        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
+        int vmin[] = new int[Math.max(w, h)];
+
+        int divsum = (div + 1) >> 1;
+        divsum *= divsum;
+        int dv[] = new int[256 * divsum];
+        for (i = 0; i < 256 * divsum; i++) {
+            dv[i] = (i / divsum);
+        }
+
+        yw = yi = 0;
+
+        int[][] stack = new int[div][3];
+        int stackpointer;
+        int stackstart;
+        int[] sir;
+        int rbs;
+        int r1 = radius + 1;
+        int routsum, goutsum, boutsum;
+        int rinsum, ginsum, binsum;
+
+        for (y = 0; y < h; y++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            for (i = -radius; i <= radius; i++) {
+                p = pix[yi + Math.min(wm, Math.max(i, 0))];
+                sir = stack[i + radius];
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+                rbs = r1 - Math.abs(i);
+                rsum += sir[0] * rbs;
+                gsum += sir[1] * rbs;
+                bsum += sir[2] * rbs;
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+            }
+            stackpointer = radius;
+
+            for (x = 0; x < w; x++) {
+
+                r[yi] = dv[rsum];
+                g[yi] = dv[gsum];
+                b[yi] = dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (y == 0) {
+                    vmin[x] = Math.min(x + radius + 1, wm);
+                }
+                p = pix[yw + vmin[x]];
+
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[(stackpointer) % div];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi++;
+            }
+            yw += w;
+        }
+        for (x = 0; x < w; x++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            yp = -radius * w;
+            for (i = -radius; i <= radius; i++) {
+                yi = Math.max(0, yp) + x;
+
+                sir = stack[i + radius];
+
+                sir[0] = r[yi];
+                sir[1] = g[yi];
+                sir[2] = b[yi];
+
+                rbs = r1 - Math.abs(i);
+
+                rsum += r[yi] * rbs;
+                gsum += g[yi] * rbs;
+                bsum += b[yi] * rbs;
+
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+
+                if (i < hm) {
+                    yp += w;
+                }
+            }
+            yi = x;
+            stackpointer = radius;
+            for (y = 0; y < h; y++) {
+                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
+                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (x == 0) {
+                    vmin[y] = Math.min(y + r1, hm) * w;
+                }
+                p = x + vmin[y];
+
+                sir[0] = r[p];
+                sir[1] = g[p];
+                sir[2] = b[p];
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[stackpointer];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi += w;
+            }
+        }
+
+        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
+
+        return (bitmap);
     }
 
 }
